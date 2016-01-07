9a07ec4fdca710174fff3a8fcb3118fee54f4bc6
Nicola Abello <nicola@infielddesign.com>
2016-01-07 13:23:49 -0800

    More fixing paths.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp ansible-aem/playbook.yml.default playbook.yml
-cp ansible-aem/roles/aem-install/vars/main.yml.default aem-install/vars/main.yml
-cp ansible-aem/roles/java/vars/main.yml.default java/vars/main.yml
+cp ansible-aem/playbook.yml.default ansible-aem/playbook.yml
+cp ansible-aem/roles/aem-install/vars/main.yml.default ansible-aem/roles/aem-install/vars/main.yml
+cp ansible-aem/roles/java/vars/main.yml.default ansible-aem/roles/java/vars/main.yml
