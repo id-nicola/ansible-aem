579975df869de209ff7ccfcd9c44bd6dccd6063b
Nicola Abello <nicola@infielddesign.com>
2016-01-07 13:16:54 -0800

    Fixed paths again.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cp ansible-aem/playbook.yml.default playbook.yml
-cp ansible-aem/aem-install/vars/main.yml.default aem-install/vars/main.yml
-cp ansible-aem/java/vars/main.yml.default java/vars/main.yml
+cp ansible-aem/roles/aem-install/vars/main.yml.default aem-install/vars/main.yml
+cp ansible-aem/roles/java/vars/main.yml.default java/vars/main.yml
