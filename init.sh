54397d669fdbc4cd2f694d79b2dd2293221e6d13
Nicola Abello <nicola@infielddesign.com>
2016-01-13 09:11:58 -0800

    Fixing scripts.

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-sudo chmod 775 get_packages-6_0.sh
-sudo chmod 775 get_packages-6_1.sh
+CURR_DIR=$1
+if [[ $# -eq 0 ]] ; then
+    CURR_DIR=${PWD}
+fi
+
+echo $CURR_DIR
+sudo chmod 775 $CURR_DIR/get_packages-6_0.sh
+sudo chmod 775 $CURR_DIR/get_packages-6_1.sh
 
 PS3='Install AEM version : '
 options=("AEM 6.0 [1]" "AEM 6.1 [2]" "Quit [3]")
@@ -8,11 +14,11 @@ select opt in "${options[@]}"
 do
     case $opt in
         "AEM 6.0 [1]")
-            ./get_packages-6_0.sh
+            exec $CURR_DIR/get_packages-6_0.sh
             break
         ;;
         "AEM 6.1 [2]")
-            ./get_packages-6_1.sh
+            exec $CURR_DIR/get_packages-6_1.sh
             break
         ;;
         "Quit [3]")
@@ -22,6 +28,6 @@ do
     esac
 done
 
-cp ansible-aem/playbook.yml.default ansible-aem/playbook.yml
-cp ansible-aem/roles/aem-install/vars/main.yml.default ansible-aem/roles/aem-install/vars/main.yml
-cp ansible-aem/roles/java/vars/main.yml.default ansible-aem/roles/java/vars/main.yml
+cp $CURR_DIR/playbook.yml.default ansible-aem/playbook.yml
+cp $CURR_DIR/roles/aem-install/vars/main.yml.default ansible-aem/roles/aem-install/vars/main.yml
+cp $CURR_DIR/roles/java/vars/main.yml.default ansible-aem/roles/java/vars/main.yml
