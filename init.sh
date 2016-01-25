6e12e78577e5311f8c65e371506be13cb55ea241
Nicola Abello <nicola@infielddesign.com>
2016-01-25 10:31:50 -0800

    Updated init.sh to reflect AEM 5.6 and Dispatcher.

--- init.sh
+++ init.sh
@@ -12,12 +12,16 @@ cp $CURR_DIR/playbook.yml.default ansible-aem/playbook.yml
 cp $CURR_DIR/roles/aem-install/vars/main.yml.default ansible-aem/roles/aem-install/vars/main.yml
 cp $CURR_DIR/roles/java/vars/main.yml.default ansible-aem/roles/java/vars/main.yml
 
-PS3='Install AEM version : '
-options=("AEM 6.0 [1]" "AEM 6.1 [2]" "Quit [3]")
+PS3='Install : '
+options=("Dispatcher [1]" "AEM 5.6 [2]" "AEM 6.0 [3]" "AEM 6.1 [4]" "Quit [5]")
 select opt in "${options[@]}"
 do
     case $opt in
-        "AEM 6.0 [1]")
+        "Dispatcher [1]")
+        ;;
+        "AEM 5.6 [2]")
+        ;;
+        "AEM 6.0 [3]")
             echo "" >> $CURR_DIR/roles/aem-install/vars/main.yml;
             echo "#AEM 6.0" >> $CURR_DIR/roles/aem-install/vars/main.yml;
             echo "aem_package_zips: ['AEM-6.0-Service-Pack-3-6.0.SP3.zip']" >> $CURR_DIR/roles/aem-install/vars/main.yml;
@@ -26,7 +30,7 @@ do
             exec $CURR_DIR/get_packages-6_0.sh;
             break;
         ;;
-        "AEM 6.1 [2]")
+        "AEM 6.1 [4]")
             echo "" >> $CURR_DIR/roles/aem-install/vars/main.yml;
             echo "#AEM 6.1" >> $CURR_DIR/roles/aem-install/vars/main.yml;
             echo "aem_package_zips: ['cq-6.1.0-hotfix-6449-1.2.zip', 'cq-6.1.0-hotfix-7085-1.0.zip', 'cq-6.1.0-hotfix-6446-1.0.zip', 'cq-6.1.0-hotfix-6500-1.5.zip', 'cq-6.1.0-hotfix-7700-2.2.zip', 'cq-6.1.0-hotfix-6972-1.0.zip', 'cq-6.1.0-hotfix-6640-1.4.zip', 'cq-6.1.0-hotfix-6680-1.2.zip', 'cq-6.1.0-hotfix-7285-1.0.zip', 'cq-6.1.0-hotfix-6570-1.3.zip', 'cq-6.1.0-featurepack-6563-1.0.0.zip']" >> $CURR_DIR/roles/aem-install/vars/main.yml;
@@ -35,7 +39,7 @@ do
             exec $CURR_DIR/get_packages-6_1.sh;
             break;
         ;;
-        "Quit [3]")
+        "Quit [5]")
             exit
         ;;
         *) echo Invalid option;;
